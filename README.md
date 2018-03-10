ed0fbac7a4132e59dde564359b1fbcd89606013b
gitPhunky <jabayquen@gmail.com>
2018-03-09 18:58:06 -0800

    initial commit
    added grabpass-pixelate and grabpass-grayscale shaders

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # phunky-unity-shaders
 A collection of shaders that I have created, tested, and used for various Unity projects
+
+A license is not currently offered with this project. Please contact for private licensing information.
---
+++ shaders/grabpass-pixelate.cs
@@ -0,0 +1,61 @@
+//This shader replicates the pixelate post processing image effect as a material shader that can be placed on meshes.
+Shader "Phunky/grab-pass-pixelate"
+{
+	Properties
+	{
+		_PixelSize("Pixel Size", Float) = 10
+	}
+
+		SubShader
+	{
+		Tags{ "Queue" = "Overlay" "IgnoreProjector" = "True" }
+		Blend Off
+		Lighting Off
+		Fog{ Mode Off }
+		ZWrite Off
+		LOD 200
+		Cull Off
+		ZTest Off
+
+		GrabPass{ "_GrabTexture" }
+
+		Pass
+	{
+		CGPROGRAM
+
+#pragma vertex vert
+#pragma fragment frag
+#include "UnityCG.cginc"
+
+		struct v2f
+	{
+		float4 pos : SV_POSITION;
+		float4 uv : TEXCOORD0;
+	};
+
+	float _PixelSize;
+
+	v2f vert(appdata_base v)
+	{
+		v2f o;
+		o.pos = UnityObjectToClipPos(v.vertex);
+		o.uv = ComputeGrabScreenPos(o.pos);
+		return o;
+	}
+
+	sampler2D _GrabTexture;
+
+	float4 frag(v2f IN) : COLOR
+	{
+		float2 steppedUV = IN.uv.xy / IN.uv.w;
+		steppedUV /= _PixelSize / _ScreenParams.xy;
+		steppedUV = round(steppedUV);
+		steppedUV *= _PixelSize / _ScreenParams.xy;
+
+		return tex2D(_GrabTexture, steppedUV);
+	}
+
+		ENDCG
+	}
+	}
+}
