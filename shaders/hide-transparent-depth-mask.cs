bae2f2cc0d2647d3064284d5452f840cf0f8e5d1
gitPhunky <jabayquen@gmail.com>
2018-03-10 23:09:21 -0800

    added transparent depth mask
    added transparent depth mask

---
+++ shaders/hide-transparent-depth-mask.cs
@@ -0,0 +1,9 @@
+﻿Shader "Phunky/hide-transparent-depth-mask" {
+	SubShader{
+		// draw before transparent render queue:
+		Tags{ "Queue" = "Transparent-1" }
+		Pass{
+		Blend Zero One // keep the image behind it
+	}
+	}
+}
